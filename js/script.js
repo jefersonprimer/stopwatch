7673a679edb6f1637482486a33d0c707a8d5b36f
jefersonprimer <jefersonpriimer@gmail.com>
2024-11-15 09:51:08 -0300

    feat: v1.1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 let secunds = 0;
 let minutes = 0;
+let hours = 0;
 let interval;
 
+function lessOfTeen(digit) {
+  if (digit < 10) {
+    return "0" + digit;
+  } else {
+    return digit;
+  }
+}
+
 function startBtn() {
+  counter();
   interval = setInterval(counter, 1000);
 }
 
@@ -14,7 +24,8 @@ function resetBtn() {
   clearInterval(interval);
   secunds = 0;
   minutes = 0;
-  document.getElementById("timer").innerText = "00:00";
+  hours = 0;
+  document.getElementById("timer").innerText = "00:00:00";
 }
 
 function counter() {
@@ -22,6 +33,12 @@ function counter() {
   if (secunds == 60) {
     minutes++;
     secunds = 0;
+    if (minutes == 60) {
+      hours++;
+      minutes = 0;
+      secunds = 0;
+    }
   }
-  document.getElementById("timer").innerText = minutes + ":" + secunds;
+  document.getElementById("timer").innerText =
+    lessOfTeen(hours) + ":" + lessOfTeen(minutes) + ":" + lessOfTeen(secunds);
 }
